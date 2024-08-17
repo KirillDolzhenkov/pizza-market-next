53b031f634a2ee85592497cc49f4833fd77b4993
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2024-08-17 15:55:57 +0300

    created topBar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { Categories, Container, SortPopup, Title, TopBar } from '@/components/shared';
+
 export default function Home() {
-  return <h1>Главная</h1>;
+  return (
+    <>
+      <Container className="mt-10">
+        <Title className="font-extrabold" size="lg" text="Все пиццы" />
+      </Container>
+      <TopBar />
+    </>
+  );
 }
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,5 +1,8 @@
 export { CartButton } from './cartButton/cartButton';
+export { Categories } from './categories/categories';
 export { Container } from './container/container';
 export { Header } from './header/header';
 export { ProfileButton } from './profileButton/profileButton';
+export { SortPopup } from './sortPopup/sortPopup';
 export { Title } from './title/title';
+export { TopBar } from './topBar/topBar';
